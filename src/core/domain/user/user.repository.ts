527646e8a9a826857f7a2a63c49a0cdad53487d7
Franco Morales Castro <franco.morales@outlook.com>
2023-10-15 17:50:35 -0300

    feat: ✨ initial app

--- src/core/domain/user/user.repository.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "./user";
-
-export interface UserRepositoryInterface {
-    findById(id: number): Promise<User | null>;
-    findByEmail(email: string): Promise<User | null>;
-    create(user: User): Promise<User>;
-}
